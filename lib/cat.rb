0afb2cef700c962b51c4511684840642fdef353a
Milos Popovic <milos_popovic@outlook.com>
2020-07-29 23:55:48 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,13 @@
 class Cat
-  # code goes here
+
+  attr_reader :name, :Owner
+  @@all = []
+
+  # def initialize(name, Owner)
+  #   @name = name
+  #   @Owner = species
+  #   @@all << self
+  # end
+
+
 end
