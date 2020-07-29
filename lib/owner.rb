cd540e461f8f52868b2861aaae594621097a9858
Milos Popovic <milos_popovic@outlook.com>
2020-07-29 23:50:59 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,16 +1,21 @@
 class Owner
 
   attr_reader :name, :species
+  @@all = []
 
   def initialize(name, species = "human")
     @name = name
     @species = species
+    @@all << self
   end
 
   def say_species
      "I am a #{species}."
   end
 
+  def self.all
+    @@all
+  end
 
 
 
