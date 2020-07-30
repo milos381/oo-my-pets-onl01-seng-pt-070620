494d30e51036e9b69d5d470d3e91e0ed9c10deda
Milos Popovic <milos_popovic@outlook.com>
2020-07-30 01:49:48 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,31 @@
+require 'pry'
 class Dog
-  # code goes here
+
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  @@all = []
+
+  def initialize(name, owner, mood = "nervous")
+    @name = name
+    @owner = owner #self.owner_name
+    @mood = mood
+    @@all << self
+  #   def initialize(name, owner)
+  #   @name = name
+  #
+  # ## you're inside a cats initialize method, name will always be referring to a cats name
+  #
+  # ## but you're creating an owner with that name right here:
+  #   @owner = Owner.new(name)
+  #   @@all << self
+  # end
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,36 @@
 class Owner
-  # code goes here
+
+  attr_accessor :cats
+  attr_reader :name, :species
+  @@all = []
+
+  def initialize(name, species = "human")
+    @name = name
+    @species = species
+    @@all << self
+    @cats = []
+
+  end
+
+  def say_species
+     "I am a #{species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def cats
+    @cats << Cat.new(name, mood = "nervous")
+  end
+
+
 end
